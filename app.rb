320d3b1429713facd6b955e5824ab31412b7a1d8
Josh Glasson <Josh.Glasson@tesco.com>
2019-04-08 15:20:42 +0100

    refactoring controller

--- app.rb
+++ app.rb
@@ -5,16 +5,14 @@ class BookmarkManager < Sinatra::Base
   get '/' do
     'Bookmark Manager'
   end
-  
+
   get '/bookmarks' do
     @bookmarks = Bookmark.all
     erb :'bookmarks'
   end
 
   post '/bookmarks' do
-    url = params['url']
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-    connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+    Bookmark.create(url: params['url'])
     redirect '/bookmarks'
   end
 
