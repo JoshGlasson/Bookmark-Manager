610e65c33ded44f5046dfbaa5d4c2201a2f26270
Josh Glasson <Josh.Glasson@tesco.com>
2019-04-08 15:19:27 +0100

    added add bookmark ability

--- app.rb
+++ app.rb
@@ -5,10 +5,21 @@ class BookmarkManager < Sinatra::Base
   get '/' do
     'Bookmark Manager'
   end
-
+  
   get '/bookmarks' do
     @bookmarks = Bookmark.all
     erb :'bookmarks'
+  end
+
+  post '/bookmarks' do
+    url = params['url']
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+    redirect '/bookmarks'
+  end
+
+  get '/bookmarks/new' do
+    erb :"bookmarks/new"
   end
 
   run! if app_file == $0
